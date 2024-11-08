5bd216790557f93604efab6d1b52f91de7462eff
Silea Horia <horia.silea@gmail.com>
2024-11-08 15:02:45 +0200

    added closeRFile statement

---
+++ src/model/statement/closeRFile.java
@@ -0,0 +1,45 @@
+package model.statement;
+
+import model.exception.DictionaryException;
+import model.exception.ExpressionException;
+import model.exception.StmtException;
+import model.expression.IExp;
+import model.state.PrgState;
+import model.type.StringType;
+import model.value.IValue;
+import model.value.StringValue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class closeRFile implements IStmt {
+    private IExp exp;
+
+    public closeRFile(IExp exp) {
+        this.exp = exp;
+    }
+
+    @Override
+    public PrgState execute(PrgState state) throws StmtException {
+        try {
+            IValue eval = this.exp.eval(state.getSymTable());
+
+            if (!eval.getType().equals(new StringType())) {
+                throw new ExpressionException("Expression is not a string");
+            }
+
+            try (BufferedReader br = state.getFileTable().lookup((StringValue) eval)){
+                br.close();
+                state.getFileTable().remove((StringValue) eval);
+                return state;
+            }
+        } catch (ExpressionException | DictionaryException | IOException e) {
+            throw new StmtException(e.getMessage());
+        }
+    }
+
+    @Override
+    public IStmt deepCopy() {
+        return new closeRFile(this.exp.deepCopy());
+    }
+}

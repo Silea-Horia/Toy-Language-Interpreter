0fe9ee4476145555cc20f83ea59a0d250066beed
916-Silea-Horia <horia.silea@stud.ubb.ro>
2024-11-15 09:01:17 +0200

    Changed the repository

--- src/repository/Repository.java
+++ src/repository/Repository.java
@@ -19,11 +19,13 @@ import model.value.StringValue;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Repository implements IRepository {
-    private java.util.List<PrgState> stateList;
+    private List<PrgState> stateList;
     private String logFilePath;
     private int  currentProgramState;
+    private IStmt initialStatement;
 
     public Repository(String logFilePath) {
         this.stateList = new ArrayList<>();
@@ -58,29 +60,29 @@ public class Repository implements IRepository {
         }
     }
 
-    private IStmt generateState1() {
-        return new CompStmt(new VarDeclStmt("v", new IntType()),
-                new CompStmt(new AssignStmt("v",new ValueExp(new IntValue(2))), new PrintStmt(new VarExp("v"))));
+    private void generateState1() {
+        this.initialStatement = new CompStmt(new VarDeclStmt("v", new IntType()),
+                            new CompStmt(new AssignStmt("v",new ValueExp(new IntValue(2))), new PrintStmt(new VarExp("v"))));
     }
 
-    private IStmt generateState2() {
-        return new CompStmt(new VarDeclStmt("a",new IntType()),
+    private void generateState2() {
+        this.initialStatement = new CompStmt(new VarDeclStmt("a",new IntType()),
                 new CompStmt(new VarDeclStmt("b",new IntType()),
                         new CompStmt(new AssignStmt("a", new ArithExp(new ValueExp(new IntValue(2)),new ArithExp(new ValueExp(new IntValue(3)), new ValueExp(new IntValue(5)), '*'), '+')),
                                 new CompStmt(new AssignStmt("b",new ArithExp(new VarExp("a"), new ValueExp(new IntValue(1)), '+')),
                                         new PrintStmt(new VarExp("b"))))));
     }
 
-    private IStmt generateState3() {
-        return new CompStmt(new VarDeclStmt("a",new BoolType()),
+    private void generateState3() {
+        this.initialStatement = new CompStmt(new VarDeclStmt("a",new BoolType()),
                 new CompStmt(new VarDeclStmt("v", new IntType()),
                         new CompStmt(new AssignStmt("a", new ValueExp(new BoolValue(true))),
                                 new CompStmt(new IfStmt(new VarExp("a"),new AssignStmt("v",new ValueExp(new
                                         IntValue(2))), new AssignStmt("v", new ValueExp(new IntValue(3)))), new PrintStmt(new VarExp("v"))))));
     }
 
-    private IStmt generateState4() {
-        return new CompStmt(new VarDeclStmt("varf",new StringType()),
+    private void generateState4() {
+        this.initialStatement =  new CompStmt(new VarDeclStmt("varf",new StringType()),
                 new CompStmt(new AssignStmt("varf", new ValueExp(new StringValue("src/files/test.in"))),
                         new CompStmt(new OpenRFile(new VarExp("varf")),
                                 new CompStmt(new VarDeclStmt("varc", new IntType()),
@@ -93,14 +95,14 @@ public class Repository implements IRepository {
 
     @Override
     public void setState(int option) {
-        IStmt initialStatementList = switch (option) {
+        switch (option) {
             case 1 -> this.generateState1();
             case 2 -> this.generateState2();
             case 3 -> this.generateState3();
             case 4 -> this.generateState4();
-            default -> new NopStmt();
+            default -> this.initialStatement = new NopStmt();
         };
         this.stateList.clear();
-        this.stateList.add(new PrgState(new ExeStack<>(), new SymTable<>(), new Out<>(), initialStatementList, new FileTable<>()));
+        this.stateList.add(new PrgState(new ExeStack<>(), new SymTable<>(), new Out<>(), this.initialStatement, new FileTable<>()));
     }
 }
